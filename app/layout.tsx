22b57d9c7b512d6e86e08ad616a090660b4203d2
Maykol Laguna <lagunamichael2@outlook.com>
2025-07-17 20:58:24 -0600

    feat: added theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+
+import { geistMono, geistSans } from "@/lib/fonts"
+import { ThemeProvider } from "@/components/theme-provider"
 
 import "./globals.css"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+import { ModeToggle } from "@/components/toggle-mode"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +18,19 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModeToggle />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
---
+++ lib/fonts.ts
@@ -0,0 +1,26 @@
+import {
+  JetBrains_Mono as FontMono,
+  Inter as FontSans,
+  Geist,
+  Geist_Mono,
+} from "next/font/google"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+
+export const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
