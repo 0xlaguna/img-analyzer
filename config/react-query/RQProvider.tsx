0f9d2f2a17d34938e930fda2079be4065bc28250
Maykol Laguna <lagunamichael2@outlook.com>
2025-07-18 00:28:18 -0600

    feat: add react query and axios utils

---
+++ config/react-query/RQProvider.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useState } from "react"
+import type { ReactNode } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+type Props = { children: ReactNode }
+
+export default function RQProvider({ children }: Props) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            retry: false,
+            staleTime: 1440,
+          },
+        },
+      })
+  )
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
---
+++ config/react-query/index.ts
@@ -0,0 +1,5 @@
+import { getQueryClient } from "./client"
+import Hydrate from "./hydrate"
+import RQProvider from "./RQProvider"
+
+export { Hydrate, RQProvider, getQueryClient }
